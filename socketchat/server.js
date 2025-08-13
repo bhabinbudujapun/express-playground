7e21a0d0a0351def3185d55bcbaf7a37ad937790
Bhabin <poon.bhabin6332@gmail.com>
2025-08-13 22:37:10 +0545

    add: initial commit, broadcasting message to all user

---
+++ socketchat/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { createServer } from "http";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server runing on PORT: 3000");
+});
