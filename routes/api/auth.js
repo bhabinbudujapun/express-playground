cac43fdb09f97b8f132a76a7abc9193916757412
Bhabin <poon.bhabin6332@gmail.com>
2025-08-13 21:17:43 +0545

    struct.: use express and multer to upload file(jpge)

--- routes/api/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ Status: "Successfully Access Auth Route...." });
-});
-
-module.exports = router;
